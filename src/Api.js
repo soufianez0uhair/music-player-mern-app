7952a565309703f6f5bb02b1313f22b5c81e03b2
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-03-01 17:59:00 +0100

    Edit Api

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,3 @@
-export const TRACKS_API = 'https://api.deezer.com/search?q=';
+export const TRACKS_API = 'https://corsproxy.io/?https://api.deezer.com/search?q=';
 export const USER_API = 'https://mern-player-api.onrender.com/api/user';
 export const FAVORITES_API = 'https://mern-player-api.onrender.com/api/v1/favTracks';
